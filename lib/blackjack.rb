8ed1c03783792e12d1bdda2a16ee0c38be8cf336
Steven Zapata <szapata427@gmail.com>
2018-09-01 19:34:17 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -31,15 +31,15 @@ def initial_round
 end	
   
 
-def hit?(current_card_total)
-    prompt_user
-    input = get_user_input
+def hit?(current_total)
+  # code hit? here	  prompt_user
+  input= get_user_input
   if input == "h"
-    current_card_total += deal_card
-  elsif input != "h" || input != "s"
-    puts invalid_command
+    current_total+= deal_card
+  elsif input == "s"
+    return current_total
   end
-end
+end	
     
   
   
