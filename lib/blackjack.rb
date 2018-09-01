8895434e7f5d46328a59996c9a934d2462c19052
Steven Zapata <szapata427@gmail.com>
2018-09-01 19:33:12 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -33,7 +33,7 @@ end
 
 def hit?(current_card_total)
     prompt_user
-    get_user_input
+    input = get_user_input
   if get_user_input == "h"
     current_card_total += deal_card
   elsif get_user_input != "h" || get_user_input != "s"
