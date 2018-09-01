c5cc1579eb3401fccf781a8e55c00c7a4baf4941
Steven Zapata <szapata427@gmail.com>
2018-09-01 19:14:39 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -28,7 +28,7 @@ def initial_round
   deal_one = deal_card
   deal_two = deal_card
   card_total = deal_one + deal_two
-  display_card_total(card_total)
+  puts display_card_total(card_total)
 end	
   
 
